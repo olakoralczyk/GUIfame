2ede56f0b4812a5680afd747ed8d0e05c992c0b7
olakoralczyk <33289489+olakoralczyk@users.noreply.github.com>
2019-02-23 21:07:40 +0100

    Add files via upload

---
+++ GUIgame/GUIgame/defines.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#define SCREEN_WIDTH 384
+#define SCREEN_HEIGHT 568
+#define SPLASH_STATE_SHOW_TIME 0.5
+#define SPLASH_SCENE_BACKGROUND_FILEPATH "Assets/Img/Splash Background.png"
+#define MAIN_MENU_BACKGROUND_FILEPATH "Assets/Img/Main Menu Background.png"
+#define GAME_BACKGROUND_FILEPATH "Assets/Img/Main Menu Background.png"
+#define PAUSE_BACKGROUND_FILEPATH "Assets/Img/res/Main Menu Background.png"
+#define MAIN_MENU_TITLE_FILEPATH "Assets/Img/Game Title.png"
+#define MAIN_MENU_PLAY_BUTTON "Assets/Img/Play Button.png"
+#define RESUME_BUTTON "Assets/Img/Resume Button.png"
+#define HOME_BUTTON "Assets/Img/Home Button.png"
+#define RETRY_BUTTON "Assets/Img/Retry Button.png"
+#define MAIN_MENU_PLAY_BUTTON_OUTER "Assets/Img/Play Button Outer.png"
+#define PAUSE_BUTTON "Assets/Img/Pause Button.png"
+#define X_PIECE_FILEPATH "Assets/Img/X.png"
+#define O_PIECE_FILEPATH "Assets/Img/O.png"
+#define X_WINNING_PIECE_FILEPATH "Assets/Img/X Win.png"
+#define O_WINNING_PIECE_FILEPATH "Assets/Img/O Win.png"
+#define GRID_SPRITE_FILEPATH "Assets/Img/Grid.png"
+#define X_PIECE 8
+#define O_PIECE 0
+#define EMPTY_PIECE -1
+#define PLAYER_PIECE X_PIECE
+#define AI_PIECE O_PIECE
+#define STATE_PLAYING 98
+#define STATE_PAUSED 97
+#define STATE_WON 96
+#define STATE_LOSE 95
+#define STATE_PLACING_PIECE 94
+#define STATE_AI_PLAYING 93
+#define STATE_DRAW 92
+#define TIME_BEFORE_SHOWING_GAME_OVER 1
---
+++ GUIgame/GUIgame/main.cpp
@@ -0,0 +1,7 @@
+#include "Game.hpp"
+#include "defines.hpp"
+
+int main()
+{
+	Ola::Game(SCREEN_WIDTH, SCREEN_HEIGHT, "GUIgame");
+}
